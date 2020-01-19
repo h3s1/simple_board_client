8bff23e16f770c57975928cc3044f33ceeaf6a0e
Geon Lee <leegun2003@gmail.com>
2020-01-19 16:10:39 +0900

    ✨ add ArticleDetail, ArticleList, ArticlePost routes

---
+++ src/routes/ArticleList.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface Props {}
+
+interface State {}
+
+export default function ArticleList<Props, State>() {
+  return <span>Article List</span>;
+}
---
+++ src/routes/ArticlePost.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface Props {}
+
+interface State {}
+
+export default function ArticlePost<Props, State>() {
+  return <span>Article Post</span>;
+}
